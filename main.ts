392a9352d5cdf07e08d50bad271690f4fc9bc943
genislloret <96112810+genislloret@users.noreply.github.com>
2021-12-17 11:06:49 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    if (brillo < 256) {
+        brillo += 51
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (brillo >= 0) {
+        brillo += -51
+    }
+})
+let brillo = 0
+led.setBrightness(0)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
 basic.forever(function () {
-	
+    led.setBrightness(brillo)
 })
